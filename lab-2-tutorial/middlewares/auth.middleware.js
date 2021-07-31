fafce36a38716939dc6c053137f5b6cf2bc8fe31
TasfiaPrima <55846643+TasfiaPrima@users.noreply.github.com>
2021-07-31 21:21:51 +0600

    view,public changed

--- lab-2-tutorial/middlewares/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    req.flash("error", "You do not have access");
-    res.redirect("/users/login");
-  }
-};
-module.exports = ensureAuthenticated;
